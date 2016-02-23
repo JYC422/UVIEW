7befbc9149a5f565afabd5fac6dd3ceb59575292
Jui-Young Chen <jyc422@gmail.com>
2016-02-23 15:15:34 -0800

    add food stars to model and put example usage in readme

--- uview_app/app/models/review.rb
+++ uview_app/app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ActiveRecord::Base
+  include ActionView::Helpers::OutputSafetyHelper
+
+  def food_rating_icons
+    raw(("<i class=\"glyphicon glyphicon-star\"></i> " * food_rating) + ("<i class=\"glyphicon glyphicon-star-empty\"></i> " * (5-food_rating)).strip)
+  end
 end
