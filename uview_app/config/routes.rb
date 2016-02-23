960c05981ccda1691a1e66455771a6e0538b8ce6
Jui-Young Chen <jyc422@gmail.com>
2016-02-23 14:54:04 -0800

    look and feel; new font; image added

--- uview_app/config/routes.rb
+++ uview_app/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   root "users#index"
   resources :users, only: [:new, :create, :show, :edit, :update]
 
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
-
 end
